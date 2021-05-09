98c55bbc4b899c39e655b39fbe26e260cb961557
NathanaelOliver <nathanaeloliver437@gmail.com>
2021-05-09 12:07:05 -0400

    First Commit

---
+++ lib/my_home_page.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'post_list.dart';
+import 'text_input_widget.dart';
+import 'post.dart';
+import 'database.dart';
+
+class MyHomePage extends StatefulWidget {
+  final FirebaseUser user;
+  MyHomePage(this.user);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Post> posts = [];
+
+  void newPost(String text) {
+    var post = new Post(text, widget.user.displayName);
+    post.setId(savePost(post));
+    this.setState(() {
+      posts.add(post);
+    });
+  }
+
+  void updatePosts() {
+    getAllPosts().then((posts) => {
+          this.setState(() {
+            this.posts = posts;
+          })
+        });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    updatePosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello World"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: PostList(this.posts, widget.user)),
+          TextInputWidget(this.newPost),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/text_input_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class TextInputWidget extends StatefulWidget {
+  final Function(String) callback;
+
+  TextInputWidget(this.callback);
+
+  @override
+  _TextInputWidgetState createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<TextInputWidget> {
+  final controller = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void send() {
+    widget.callback(controller.text);
+    controller.clear();
+    FocusScope.of(context).unfocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        controller: this.controller,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.message),
+          labelText: "Type here",
+          suffixIcon: IconButton(
+            icon: Icon(Icons.send),
+            tooltip: "Post message",
+            onPressed: this.send,
+          ),
+        ));
+  }
+}
